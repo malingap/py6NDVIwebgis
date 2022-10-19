6bdf49f4955398e933642b4515d5194b0077d957
malingap <malingagoldsliit@gmail.com>
2022-10-19 11:55:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ apps = [
     {"func": home.app, "title": "Home", "icon": "house"},
     {"func": heatmap.app, "title": "Heatmap", "icon": "map"},
     {"func": upload.app, "title": "Upload", "icon": "cloud-upload"},
+    {"func": dem.app, "title": "DEM", "icon": "map"},
 ]
 
 titles = [app["title"] for app in apps]
